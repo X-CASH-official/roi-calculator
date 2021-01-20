27213c51a181424f25e5f14256a3e72e82d85d6d
Radu Radu <radu@radu.cat>
2021-01-20 22:23:31 +0100

    remove tabs

--- api/v1/index.php
+++ api/v1/index.php
@@ -6,38 +6,38 @@ require('../../src/Funcs.php');
 $router = new \Bramus\Router\Router();
 
 $router->set404(function () {
-	header('HTTP/1.1 404 Not Found');
-	$return	= [];
-	$return['error']	= true;
-	$return['message']	= "404 Not Found";
+    header('HTTP/1.1 404 Not Found');
+    $return	= [];
+    $return['error']	= true;
+    $return['message']	= "404 Not Found";
 
-	echo json_encode($return, JSON_PRETTY_PRINT);
+    echo json_encode($return, JSON_PRETTY_PRINT);
 });
 
 $router->before('GET', '/.*', function () {
-	header('Content-Type: application/json');
+    header('Content-Type: application/json');
 });
 
 
 $router->get('/', function () {
-	header('HTTP/1.1 204 No Content');
-	$return	= [];
-	$return['error']	= true;
-	$return['message']	= "204 No Content";
+    header('HTTP/1.1 204 No Content');
+    $return	= [];
+    $return['error']	= true;
+    $return['message']	= "204 No Content";
 
-	echo json_encode($return, JSON_PRETTY_PRINT);
+    echo json_encode($return, JSON_PRETTY_PRINT);
 });
 
 $router->get('/delegates(/[A-z0-9_\-\.]+)?(/[A-z0-9_\-\.]+)?(/\d+)?', function ($name = null, $amount = null, $days = null) {
-	$amount	= ($amount) ? $amount : "2M";
-	$days	= ($days) ? $days : 30;
-	$name	= ($name) ? $name : "all";
+    $amount	= ($amount) ? $amount : "2M";
+    $days	= ($days) ? $days : 30;
+    $name	= ($name) ? $name : "all";
 
-	if($name == "all"){
-		GetDelegates($amount, $days);
-	} else {
-		GetDelegate($name, $amount, $days);
-	}
+    if($name == "all"){
+        GetDelegates($amount, $days);
+    } else {
+        GetDelegate($name, $amount, $days);
+    }
 }); 
 
 $router->run();
